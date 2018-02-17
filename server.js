604adae51298139b06c4c058d5a800c91ac1619d
Philip B <philipblaquiere@gmail.com>
2018-02-18 10:51:43 +1100

    removed func on slack.listen explicit no use ws

--- server.js
+++ server.js
@@ -271,6 +271,4 @@ function titleCase(str) {
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
  
-slack.listen(server_port, server_ip_address, function () {
-  console.log( "Listening on " + server_ip_address + ", port " + server_port )
-});
+slack.listen(server_port, server_ip_address, false);
