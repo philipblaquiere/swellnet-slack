567038ae0528d676a9743372ae40b7e237a081c7
Philip B <philipblaquiere@gmail.com>
2018-03-01 18:29:12 +1100

    reduced node package target to 4.3.2

--- server.js
+++ server.js
@@ -268,7 +268,7 @@ function titleCase(str) {
   return str.join(' ');
 }
 
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+const PORT = process.env.PORT || 8080;
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
  
-slack.listen(server_port, server_ip_address, false);
+slack.listen(PORT, server_ip_address, false);
