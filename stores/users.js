e4d215c8a7581ae11e1231202ac57cb4273706c5
Philip Blaquiere <philip.blaquiere@4mation.local>
2018-03-02 10:18:07 +1100

    Fixing Low db issues

--- stores/users.js
+++ stores/users.js
@@ -1,24 +1,41 @@
-const flat = require('node-flat-db')
-const storage = require('node-flat-db/file-sync')
- 
-const db = flat('.data/users.json', { storage })
+const low = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync')
+
+const adapter = new FileSync('./db.json')
+const db = low(adapter)
  
 const methods = {};
 
+let hasUsers = db.has('users').value();
+
+if(!hasUsers) {
+  db.set('users', [])
+    .write()
+}
+
 methods.getUser = function(userId) {
-  console.log(db.object.users);
-  let user = db('users').find({ Id: userId });
+  let user = db
+    .get('users')
+    .find({ Id: userId })
+    .value();
   
   if(user !== undefined) {
     return user;
   }
   
   return undefined;
-}
-
+} 
+ 
 methods.setUser = function(userId, place) { 
-  db('users').remove({ Id: userId })
-  db('users').push({ Id: userId, place: place });
+  db
+    .get('users')
+    .remove({ Id: userId })
+    .write();
+  
+  db
+    .get('users')
+    .push({ Id: userId, place: place })
+    .write();
 }
 
 exports.data = methods;
